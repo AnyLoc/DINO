5bfa520e24ed55e5100e9b65e2edb2c418f71993
Avneesh Mishra <123avneesh@gmail.com>
2023-10-28 21:48:09 +0530

    Added provision to init model on GPU and to check cache of downloaded cluster centers

--- hubconf.py
+++ hubconf.py
@@ -10,7 +10,7 @@ dependencies = [
 import os
 import torch
 from torch import nn
-from model_wrapper import AnyLocVladDinov2
+import model_wrapper as mw
 
 
 # %%
@@ -32,7 +32,8 @@ BASER_URL = "https://github.com/AnyLoc/DINO/releases/download/v1"
 def get_vlad_model(backbone: str = "DINOv2", 
             vit_model: str = "ViT-G/14", vit_layer: int = 31, 
             vit_facet: str = "Value", num_c: int = 32, 
-            domain: str = "indoor") -> nn.Module:
+            domain: str = "indoor", device: torch.device = "cpu") \
+            -> nn.Module:
     """
         Load an AnyLoc-VLAD-[backbone] model from torch.hub
         The default settings are for AnyLoc-VLAD-DINOv2; and the
@@ -50,6 +51,7 @@ def get_vlad_model(backbone: str = "DINOv2",
         - num_c (int):      Number of cluster centers to use (for
                             VLAD clustering).
         - domain (str):     Domain for cluster centers.
+        - device (torch.device):    Device for model; "cpu" or "cuda"
         
         Notes:
         - All string arguments are converted to lower case.
@@ -74,8 +76,12 @@ def get_vlad_model(backbone: str = "DINOv2",
     if os.path.isdir(loc_path) == False:    # Create if not there
         os.makedirs(loc_path)
     print(f"Storing (torch.hub) cache in: {loc_path}")
-    cc_path = f"{loc_path}/{cc_fname}"
-    torch.hub.download_url_to_file(f"{BASER_URL}/{cc_fname}", cc_path)
+    cc_path = f"{loc_path}/{cc_fname}"  # Path for cluster centers
+    if os.path.isfile(cc_path):
+        print(f"Cluster centers already downloaded: {cc_path}")
+    else:
+        torch.hub.download_url_to_file(f"{BASER_URL}/{cc_fname}", 
+                cc_path)
     c_centers = torch.load(cc_path)
     assert c_centers.shape[0] == num_c, "Cluster centers corrupted"
     
@@ -92,10 +98,11 @@ def get_vlad_model(backbone: str = "DINOv2",
         - [ ] Test with AnyLoc-VLAD-DINOv2
     """
     model = nn.Identity()   # Placeholder
+    device = torch.device(device)
     if backbone == "dinov2":
-        model = AnyLocVladDinov2(c_centers, 
+        model = mw.AnyLocVladDinov2(c_centers, 
                 dino_model=f"{backbone}_{vit_model}", layer=vit_layer,
-                facet=vit_facet, num_c=num_c)
+                facet=vit_facet, num_c=num_c, device=device)
     else:
         raise NotImplementedError(f"{backbone = } not implemented!")
     return model
--- model_wrapper.py
+++ model_wrapper.py
@@ -17,23 +17,43 @@ class AnyLocVladDinov2(nn.Module):
     """
     def __init__(self, c_centers: torch.Tensor, 
                 dino_model: str = "dinov2_vitg14", layer: int = 31, 
-                facet: str = "value", num_c: int = 32):
+                facet: str = "value", num_c: int = 32, 
+                device: torch.device = "cpu"):
         super().__init__()
         # DINOv2 feature extractor
-        self.dino_extractor = DinoV2ExtractFeatures(dino_model, layer,
-                facet)
+        self.dino_model = dino_model
+        self.layer = layer
+        self.facet = facet
+        self.device = torch.device(device)
+        self.dino_extractor = self._get_dino_extractor()
         # VLAD clustering
         self.vlad = VLAD(num_c)
         self.vlad.c_centers = c_centers # Load cluster centers
         self.vlad.fit(None) # Load the database (vocabulary/c_centers)
     
+    # Extractor
+    def _get_dino_extractor(self):
+        return DinoV2ExtractFeatures(
+            dino_model=self.dino_model, layer=self.layer, 
+            facet=self.facet, device=self.device)
+    
+    # Move DINO model to device
+    def to(self, device: torch.device):
+        self.device = torch.device(device)
+        self.dino_extractor = self._get_dino_extractor()
+    
+    # Wrapper around CUDA
+    def cuda(self):
+        self.to("cuda")
+    
     # Forward pass
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         img_pt = x
         # TODO: Add batching feature
-        assert len(img_pt.shape) == 4 and img_pt.shape[0] == 1 and \
-                img_pt.shape[1] == 3, "Pass only single RGB image"
+        assert len(img_pt.shape) == 4 and img_pt.shape[:2] == (1, 3),\
+                "Pass only single RGB image"
+        img_pt = img_pt.to(self.device)
         # Extract features
         ret = self.dino_extractor(img_pt)
-        gd = self.vlad.generate(ret[0])
-        return gd
+        gd = self.vlad.generate(ret[0].cpu())   # VLAD on CPU only!
+        return gd.to(self.device)
